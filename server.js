5acaa9beb004c1079e9c00501feb0075e62f4bb8
Frank B Greco Jr <fbgrecojr@me.com>
2016-03-03 09:57:33 -0600

    changes.. lol

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-const app       = require('express')(),
-      twilio    = require('twilio'),
-      authToken = '2435e46cfff31113bd81eed73497cafb',
-      sid       = 'AC1582bdee16226c1bfff1f328c48978be',
-      number    = '+14144090913',
+const express   = require('express'),
+      app       = express(),
       port      = process.env.PORT || 8080;
 
-var   client    = new twilio.RestClient(authToken, sid);
 
+app.use(express.static(__dirname + '/'));
 
-app.get('/', function(req, res){
-    res.send("Welcome to Express!!!");
-});
+/*app.get('/', function(req, res){
+    var options = {
+      root: __dirname + '/'
+    };
 
-app.post('/text', function(req, res){
-    client.sms.messages.create({
-        to: '+14144779013',
-        from: number,
-        body: 'req.body.message'
-    }, function(error, message) {
-        if (!error) {
-            console.log('Success! The SID for this SMS message is:');
-            console.log(message.sid);
-
-            console.log('Message sent on:');
-            console.log(message.dateCreated);
-        } else {
-            console.log('Oops! There was an error.');
-            throw error;
-        }
+    res.sendFile('index.html', options, function (err) {
+      if (err) {
+        console.log(err);
+        res.status(err.status).end();
+      }
+      else {
+        console.log('Sent: index.html');
+      }
     });
-});
+});*/
 
 app.listen(port, function(){
     console.log("LISTENING ON PORT " + port);
